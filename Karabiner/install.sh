980fbaf39f1adb48ccbf7a16f7b40a536f775a46
Mickel Andersson <andersson.mickel@gmail.com>
2015-07-12 19:11:21 +0200

    use absolute path when symlinking

--- Karabiner/install.sh
+++ Karabiner/install.sh
@@ -1,1 +1,14 @@
-ln -s ~/dev/tools/dotfiles/Karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+#! /bin/sh
+
+echo "Installing Karabiner configuration"
+
+dir=`dirname $0`
+
+# Add symlinks
+ln -s -f ~/dev/tools/dotfiles/Karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+
+# Configure Karbiner using the Karbiner CLI
+$dir/Karabiner-default.sh
+
+echo "Karabiner configuration installed"
+echo "Create a new profile for external and run Karabiner-external.sh"
