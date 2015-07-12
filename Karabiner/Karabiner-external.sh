8ad5601f545c7e7e0c41c5b024a4e73cb9dcf20c
Mickel Andersson <andersson.mickel@gmail.com>
2015-07-12 19:17:42 +0200

    add doublepresscommandQ to external as well

--- Karabiner/Karabiner-external.sh
+++ Karabiner/Karabiner-external.sh
@@ -2,6 +2,8 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set private.switch_profile 1
