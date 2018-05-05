21c67f8efd5b2fda1d291b542011dbe0eb293e32
Mickel <andersson.mickel@gmail.com>
2018-05-05 20:16:35 +0200

    Update Karabiner config to Karabiner-Elements

--- Karabiner/Karabiner-default.sh
+++ Karabiner/Karabiner-default.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+cli=/Applications/Karabiner-Elements.app/Contents/MacOS/Karabiner-Elements
 
 $cli set remap.doublepresscommandQ 1
 /bin/echo -n .
--- Karabiner/install.sh
+++ Karabiner/install.sh
@@ -5,7 +5,7 @@ echo "Installing Karabiner configuration"
 dir=`dirname $0`
 
 # Add symlinks
-ln -s -f ~/dev/tools/dotfiles/Karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+ln -s -f ~/dev/tools/dotfiles/Karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 
 # Configure Karbiner using the Karbiner CLI
 $dir/Karabiner-default.sh
