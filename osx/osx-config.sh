2da7c084abe530ecfa590f4de179681108eec565
Mickel <andersson.mickel@gmail.com>
2018-10-12 16:10:26 +0200

    Change OSX key repeat speed

--- osx/osx-config.sh
+++ osx/osx-config.sh
@@ -24,7 +24,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write -g InitialKeyRepeat -int 20
+defaults write NSGlobalDomain KeyRepeat -int 10
 
 # Disable auto-correct
 # defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
