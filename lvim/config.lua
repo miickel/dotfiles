a6527fad257bf66744a5c8cbd18fc31262512d5a
Mickel Andersson <mickel@waitwhile.com>
2022-11-07 08:44:39 +0100

    Remove deprecated config

--- lvim/config.lua
+++ lvim/config.lua
@@ -78,7 +78,6 @@ lvim.builtin.which_key.mappings["k"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git/" }
